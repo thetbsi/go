02dec995882c5a1113c38a0cde9efb79fa2d5ce0
thetbsi <maytt4b567@gmail.com>
2016-03-16 20:31:00 -0400

    save

---
+++ dice/src/dice/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	c "github.com/skilstak/go/colors"
+
+)
+
+func Dice() {
+sides := []
+const sides('''
+  ------
+ |      |
+ |  一  |
+ |     1|
+  ------
+''')
+
+const sides('''
+  ------
+ |      |
+ |  二  |
+ |     2|
+  ------
+''')
+
+const sides('''
+  ------
+ |      |
+ |  三  |
+ |     3|
+  ------
+''')
+
+const sides('''
+  ------
+ |      |
+ |  四  |
+ |     4|
+  ------
+''')
+
+const sides('''
+  ------
+ |      |
+ |  五  |
+ |     5|
+  ------
+''')
+
+const sides('''
+  ------
+ |      |
+ |  六  | 
+ |     6|
+  ------
+''')
+
